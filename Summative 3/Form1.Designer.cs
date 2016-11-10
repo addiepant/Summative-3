9b5ae60a845f8521688cf462a22f711e1b8b9ca8
addiepant <aadhpant936@ed.amdsb.ca>
2016-11-10 15:14:29 -0500

    Add project files.

---
+++ Summative 3/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Summative_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.Font = new System.Drawing.Font("Gabriola", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.FloralWhite;
+            this.outputLabel.Location = new System.Drawing.Point(12, 41);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(436, 335);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "Label 1";
+            // 
+            // redLabel
+            // 
+            this.redLabel.BackColor = System.Drawing.Color.Transparent;
+            this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(107, 344);
+            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(257, 23);
+            this.redLabel.TabIndex = 4;
+            this.redLabel.Text = "label1";
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.BackColor = System.Drawing.Color.Transparent;
+            this.greenLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenLabel.ForeColor = System.Drawing.Color.White;
+            this.greenLabel.Location = new System.Drawing.Point(107, 390);
+            this.greenLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(257, 23);
+            this.greenLabel.TabIndex = 5;
+            this.greenLabel.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(80, 345);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "     ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(80, 391);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "     ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(516, 477);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Christmas choose your adventure";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Summative 3/Form1.cs
@@ -0,0 +1,210 @@
+﻿/// created by : (M)Addie
+/// date       : November 7, 2016
+/// description: A basic text adventure game engine
+/// 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Summative_3
+{
+
+    public partial class Form1 : Form
+    {
+        System.Media.SoundPlayer startSoundPlayer = new System.Media.SoundPlayer(@"C:\Windows\Media\chord.wav");
+
+        Random randGen = new Random();
+
+        int scene = 1;  // tracks what part of the game the user is at
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //TODO display initial message and options
+            outputLabel.Text = "You accidentally ran over Santa so now you have to deliver all these presents in one night! Do you take on this challenge? ";
+            redLabel.Text = "Yes";
+            greenLabel.Text = "No";
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            startSoundPlayer.Play();
+
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 1)
+                {
+                    scene = 2;
+                }
+                else if (scene == 2)
+                {
+                    scene = 5;
+                }
+                else if (scene == 4)
+                {
+                    scene = 6;
+                }
+                else if (scene == 6)
+                {
+                    scene = 9;
+                }
+                else if (scene == 8)
+                {
+                    scene = 10;
+                }
+                else if (scene == 3)
+                {
+                    scene = 1;
+                }
+                else if (scene == 5)
+                {
+                    scene = 1;
+                }
+                else if (scene == 7)
+                {
+                    scene = 1;
+                }
+                else if (scene == 9)
+                {
+                    scene = 1;
+                }
+                else if (scene == 10)
+                {
+                    scene = 1;
+                }
+                else if (scene == 11)
+                {
+                    scene = 1;
+                }
+
+            }
+            else if (e.KeyCode == Keys.B)  //green button press
+            {
+                if (scene == 1)
+                {
+                    scene = 2;
+                }
+                else if (scene == 2)
+                {
+                    int chance = randGen.Next(1, 101);
+
+                    if (chance < 80)
+                    {
+                        scene = 4;
+                    }
+
+                    else if (chance > 80)
+                    {
+                        scene = 5;
+                    }
+                }
+                else if (scene == 4)
+                {
+                    scene = 7;
+                }
+                else if (scene == 6)
+                {
+                    scene = 8;
+                }
+                else if (scene == 8)
+                {
+                    scene = 11;
+                }
+                else if (scene == 3)
+                {
+                    this.Close();
+                }
+                else if (scene == 5)
+                {
+                    this.Close();
+                }
+                else if (scene == 7)
+                {
+                    this.Close();
+                }
+                else if (scene == 9)
+                {
+                    this.Close();
+                }
+                else if (scene == 10)
+                {
+                    this.Close();
+                }
+                else if (scene == 11)
+                {
+                    this.Close();
+                }
+
+
+
+            }
+
+            switch (scene)
+            {
+                case 1:  //start scene  
+                    break;
+                case 2:
+                    outputLabel.Text = "The elves are slacking off and wont build the toys and you only have 23 hours left till christmas eve!!!Do you threaten them or ask them nicely?";
+                    redLabel.Text = "Threaten them";
+                    greenLabel.Text = "Ask them nicely";
+                    break;
+                case 3:
+                    outputLabel.Text = "You get kicked out of Santa's workshop. You're stranded alone in the North Pole. You die.";
+                    redLabel.Text = "Restart";
+                    greenLabel.Text = "Close Game";
+                    break;
+                case 4:
+                    outputLabel.Text = "The elves grudgingly do their job, but... oh no! Prancer dies! Do you go ahead with it and make Rudolph take Prancer's place or get a baby reindeer to do so?";
+                    redLabel.Text = "Baby Reindeer";
+                    greenLabel.Text = "Rudolph";
+                    break;
+                case 5:
+                    outputLabel.Text = "You're on the naughty list now :) Guess you were always meant to be a loser ";
+                    redLabel.Text = "Restart";
+                    greenLabel.Text = "Close Game";
+                    break;
+                case 6:
+                    outputLabel.Text = "The baby reindeer practices and is surprisingly amazing at it. However, all the presents still aren't ready!!! Do you give the rest of the kids coal or give the rest of the kids money ?";
+                    redLabel.Text = "Coal";
+                    greenLabel.Text = "Money";
+                    break;
+                case 7:
+                    outputLabel.Text = "Rudolph crashes and breaks his leg! Now Christmas is ruined thanks to you! You really do ruin everything you touch...";
+                    redLabel.Text = "Restart";
+                    greenLabel.Text = "Close Game";
+                    break;
+                case 8:
+                    outputLabel.Text = "The elves admire your kindness, so they decide to speed up and get it done! Just one more task. Do you start delivering presents from the East or the West?";
+                    redLabel.Text = "East";
+                    greenLabel.Text = "West";
+                    break;
+                case 9:
+                    outputLabel.Text = "Wow can't believe you're Satan. ";
+                    redLabel.Text = "Restart";
+                    greenLabel.Text = "Close Game";
+                    break;
+                case 10:
+                    outputLabel.Text = "Great Job! You won the game and you have a good understanding of how time zones work! You're not stupid!";
+                    redLabel.Text = "Restart";
+                    greenLabel.Text = "Close Game";
+                    break;
+                case 11:
+                    outputLabel.Text = "Yikes, why are you like this?";
+                    redLabel.Text = "Restart";
+                    greenLabel.Text = "Close Game";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
+           
